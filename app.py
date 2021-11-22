79f0abfa4b1ed21122683e840a42729ac56311b7
hermeslapa <90638439+hermeslapa@users.noreply.github.com>
2021-11-22 10:21:58 -0300

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def job():
     kafka_consumer = FlinkKafkaConsumer(
         topics='transactions-topic',
         deserialization_schema=deserialization_schema,
-        properties={'bootstrap.servers': '10.36.0.2:9092',
+        properties={'bootstrap.servers': 'kafk-svc.default:9092',
         'group.id': 'test_group'})
     ds = env.add_source(kafka_consumer)
 
