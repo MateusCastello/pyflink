5a542b42a6a894f330a99cdbe04f90fedc6020c6
hermeslapa <90638439+hermeslapa@users.noreply.github.com>
2021-12-06 13:20:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import json
 def job():
     env = StreamExecutionEnvironment.get_execution_environment()
     env.set_runtime_mode(execution_mode=RuntimeExecutionMode.STREAMING)
-    env.enable_checkpointing(1000)
+    env.enable_checkpointing(5000)
     env.get_checkpoint_config().set_max_concurrent_checkpoints(1)
     t_env = StreamTableEnvironment.create(env)
 
