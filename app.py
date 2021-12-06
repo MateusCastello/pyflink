c133e215fc17e15f811492acb63f2c5c9a273079
hermeslapa <90638439+hermeslapa@users.noreply.github.com>
2021-12-06 13:58:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from pyflink.common.typeinfo import Types
-from pyflink.datastream.connectors import FlinkKafkaConsumer
+from pyflink.datastream.connectors import FlinkKafkaConsumer, JdbcConnectionOptions, JdbcSink
 from pyflink.common.serialization import SimpleStringSchema
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.table import StreamTableEnvironment
@@ -7,10 +7,11 @@ from pyflink.datastream.execution_mode import RuntimeExecutionMode
 import json
 
 # Teste do job usando DataStream API
+
 def job():
     env = StreamExecutionEnvironment.get_execution_environment()
     env.set_runtime_mode(execution_mode=RuntimeExecutionMode.STREAMING)
-    env.enable_checkpointing(5000)
+    env.enable_checkpointing(1000)
     env.get_checkpoint_config().set_max_concurrent_checkpoints(1)
     t_env = StreamTableEnvironment.create(env)
 
@@ -20,13 +21,13 @@ def job():
         topics='TB_NF',
         deserialization_schema=deserialization_schema,
         properties={'bootstrap.servers': 'kafka-service:9092',
-        'group.id': 'test_group',
-        'startup-mode':'earliest-offset'}
-        )
+                    'group.id': 'test_group',
+                    'startup-mode': 'earliest-offset'}
+    )
 
     ds = env.add_source(kafka_consumer)
     ds = ds.map(lambda x: list(json.loads(x)['payload']['after'].values()),
-    output_type=Types.ROW([Types.INT() ,Types.SQL_TIMESTAMP() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.STRING() ,Types.INT() ,Types.INT() ,Types.SQL_TIMESTAMP() ,Types.SQL_TIMESTAMP() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.STRING() ,Types.INT() ,Types.STRING() ,Types.STRING() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.STRING() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.SQL_TIMESTAMP() ,Types.SQL_TIMESTAMP() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.SQL_TIMESTAMP() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.STRING() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.SQL_TIMESTAMP() ,Types.SQL_TIMESTAMP() ,Types.SQL_TIMESTAMP() ,Types.INT() ,Types.INT() ,Types.SQL_TIMESTAMP() ,Types.INT() ,Types.INT() ,Types.STRING() ,Types.SQL_TIMESTAMP() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.INT() ,Types.SQL_TIMESTAMP()]))
+                output_type=Types.ROW([Types.INT(), Types.SQL_TIMESTAMP(), Types.INT(), Types.INT(), Types.INT(), Types.STRING(), Types.INT(), Types.INT(), Types.SQL_TIMESTAMP(), Types.SQL_TIMESTAMP(), Types.INT(), Types.INT(), Types.INT(), Types.STRING(), Types.INT(), Types.STRING(), Types.STRING(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.STRING(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.SQL_TIMESTAMP(), Types.SQL_TIMESTAMP(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.SQL_TIMESTAMP(), Types.INT(), Types.INT(), Types.INT(), Types.STRING(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.SQL_TIMESTAMP(), Types.SQL_TIMESTAMP(), Types.SQL_TIMESTAMP(), Types.INT(), Types.INT(), Types.SQL_TIMESTAMP(), Types.INT(), Types.INT(), Types.STRING(), Types.SQL_TIMESTAMP(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.SQL_TIMESTAMP()]))
     # Saída
     t_env.execute_sql('''
                     CREATE TABLE SINK (
@@ -86,7 +87,7 @@ def job():
                                         vl_icms_nf int ,
                                         vl_ipi_nf int ,
                                         vl_frete int ,
-                                        vl_seguro int ,
+                                        vl_seguro int ,JdbcSink
                                         vl_base_frete int ,
                                         vl_icms_frete int ,
                                         pc_icms_frete int ,
@@ -137,11 +138,22 @@ def job():
                         'connector.password' = 'postgres_password',
                         'connector.write.flush.interval' = '1s'
                     )''')
-    table = t_env.from_data_stream(ds)
+
+    jdbc_options = JdbcConnectionOptions.JdbcConnectionOptionsBuilder() \
+        .with_user_name("mantapostgres") \
+        .with_password("postgres_password") \
+        .with_url("dbc:postgresql://mantabase.c0uugfnq0yzw.us-east-1.rds.amazonaws.com:5432/mantabase") \
+        .build()
+
+    ds.add_sink(JdbcSink.sink("""
+    insert sink(vl_iss,dt_fechto_credenciada,cd_credenciada,vl_subsidio_empresa,vl_pbm_reembolso,nr_cnpj_cgc,cd_operador_venda,cd_campanha,dt_atualizacao,dt_credenciada_fechto,cd_motivo_transferencia,cd_tp_cupom,nr_coo,nr_serie_nf,cd_nfe_tp_serie,cd_chave_acesso_nfe,cd_totalizador,vl_pis,vl_cofins,vl_base_pis,vl_base_cofins,cd_resposta_pesquisa,cd_tipo_transacao,vl_contabil,nr_trilha_cartao,vl_icms_desonerado_nf,vl_base_icms_fcp_nf,vl_base_icms_st_fcp_nf,vl_base_icms_st_fcp_ant_nf,vl_base_icms_st_gare_nf,vl_icms_fcp_nf,vl_icms_st_fcp_nf,vl_icms_st_fcp_ant_nf,vl_icms_st_gare_nf,vl_troco_nf,cd_tp_trib_cliente,cd_televenda,cd_tipo_pdv,cd_tp_registro,cd_filial,cd_transacao_cliente,cd_origem_nf,cd_forma_pagto,id_cliente,nr_seque,nr_cupom,nr_sequencia_cupom,nr_caixa,qt_pontos,vl_nf,st_transmissao,cd_ent_filant,nr_nf,vl_icms_nf,vl_ipi_nf,vl_frete,vl_seguro,vl_base_frete,vl_icms_frete,pc_icms_frete,vl_base_reduzida,vl_base_icms,vl_sinal_pontos,vl_sinal_brindes,vl_sinal_saldo,dt_fechto,dt_faturamento,vl_sinal_estoque,vl_nf_repasse,fl_venda_rg,vl_total_custo,vl_sinal_estoque_indisp_origem,dt_timestamp,nr_autorizacao,cd_tp_nf,cd_empresa_venda_vinculada,st_faturamento_convenio,cd_operador,st_transmissao_terceiros,nr_autorizacao_pbm,vl_glosa_pbm_repasse,vl_glosa_pbm_subsidio,vl_glosa_convenio,dt_glosa_convenio,dt_glosa_pbm_subsidio,dt_glosa_pbm_repasse,cd_pbr,vl_estorno_glosa_convenio,dt_estorno_glosa_convenio,cd_tipo_glosa,cd_tipo_glosa_pbmr,cds_chave_acesso_nfe,dt_confirmacao_tracking,id_nf,cd_operacao_fiscal,cd_filial_origem,cd_filial_destino,dt_evento)VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""",type_info=Types.ROW([Types.INT(), Types.SQL_TIMESTAMP(), Types.INT(), Types.INT(), Types.INT(), Types.STRING(), Types.INT(), Types.INT(), Types.SQL_TIMESTAMP(), Types.SQL_TIMESTAMP(), Types.INT(), Types.INT(), Types.INT(), Types.STRING(), Types.INT(), Types.STRING(), Types.STRING(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.STRING(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.SQL_TIMESTAMP(), Types.SQL_TIMESTAMP(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.SQL_TIMESTAMP(), Types.INT(), Types.INT(), Types.INT(), Types.STRING(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.SQL_TIMESTAMP(), Types.SQL_TIMESTAMP(), Types.SQL_TIMESTAMP(), Types.INT(), Types.INT(), Types.SQL_TIMESTAMP(), Types.INT(), Types.INT(), Types.STRING(), Types.SQL_TIMESTAMP(), Types.INT(), Types.INT(), Types.INT(), Types.INT(), Types.SQL_TIMESTAMP()]),jdbc_connection_options=jdbc_options))
+
+    #table = t_env.from_data_stream(ds)
     #t_env.create_temporary_view("InputTable", table)
     #t_env.execute_sql("INSERT INTO SINK SELECT * FROM InputTable")
-    table.execute_insert("SINK").wait()
-    #t_env.execute('tb_nf')
+    # table.execute_insert("SINK").wait()
+    env.execute('tb_nf')
+
 
 if __name__ == '__main__':
     job()
